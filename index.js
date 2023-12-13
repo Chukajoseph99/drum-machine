88f3aa42c866293ce66e344b5cddfc09b6b48ec2
chukajoseph99 <chukajoseph99@gmail.com>
2023-12-13 08:46:29 +0100

    Develop drum machine using html css and javascript

---
+++ index.js
@@ -0,0 +1,44 @@
+document.querySelector(".q").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Heater-1-Q.mp3");
+    audio.play();
+})
+
+document.querySelector(".w").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Heater-2-W.mp3");
+    audio.play();
+})
+
+document.querySelector(".e").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Heater-3-E.mp3");
+    audio.play();
+})
+
+document.querySelector(".a").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Heater-4_1-A.mp3");
+    audio.play();
+})
+
+document.querySelector(".s").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Clap-S.mp3");
+    audio.play();
+})
+
+document.querySelector(".d").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Open-HH-D.mp3");
+    audio.play();
+})
+
+document.querySelector(".z").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Kick_n_Hat-Z.mp3");
+    audio.play();
+})
+
+document.querySelector(".x").addEventListener("click", function(){
+    let audio = new Audio("Sounds/RP4_KICK_1-X.mp3");
+    audio.play();
+})
+
+document.querySelector(".c").addEventListener("click", function(){
+    let audio = new Audio("Sounds/Closed-HH-C.mp3");
+    audio.play();
+})
